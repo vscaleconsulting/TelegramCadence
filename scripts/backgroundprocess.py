ba0a7f6b444b4d278c4cbe622a630ab07d3ab05a
NotVaishav <vaishavdhepe@gmail.com>
2021-07-08 15:07:19 +0530

    Time value fixed

--- scripts/backgroundprocess.py
+++ scripts/backgroundprocess.py
@@ -1,7 +1,8 @@
+from datetime import timedelta
+
 from background_task import background
-from datetime import timedelta, timezone, tzinfo, datetime
 from django.utils import timezone
-from cadence import models
+
 from .functions import send_message
 
 
@@ -12,18 +13,13 @@ def schedule_message(session, group_name, message):
 
 def schedule_cadence(cadence, group_name, start_time):
     print('Scheduling')
-    now = datetime.now() - timedelta(hours=12)
-    now = now.replace(tzinfo=None)
-    start_time = start_time.replace(tzinfo=None)
+    now = timezone.localtime()
+
     messages = cadence.messagescript_set.all()
-    delay = timedelta(seconds=0)
     for message in messages:
         session = message.account.sess_str
         delay = timedelta(days=message.time_days, hours=message.time_hours,
-                                   minutes=message.time_minutes, seconds=message.time_seconds)
+                          minutes=message.time_minutes, seconds=message.time_seconds)
         message = message.message
         start_time += delay
-        print(start_time, start_time.tzinfo)
-        print(now, now.tzinfo)
-        print(start_time-now)
-        schedule_message(session, group_name, message, schedule=start_time-now)
+        schedule_message(session, group_name, message, schedule=start_time - now)
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,15 +1,15 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
 import asyncio
 from datetime import datetime
 
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
 
 debug = True
+
 
 def send_message(sender_session, receiver, message):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    # sess_str = '1ApWapzMBu4bPq_KDVCy4vb0Y0V9IOjCcK3dQkKmIp3CL3adfUpeKznS5mzyYsM8rmwvlrWKAY8XwH1N3WqrHYfMNpXxaKuh-km9NUCwCpU9DeAbr07B3HfkypPzC7RM_mJIQzLB0h4CngnCwNJEFXva-AUlXPhcI2QLWgEaBVfce-Uys9UZ9ETL1QoMbZrkZzHKDa_aB3s2McSkkc9H4wTJHzfHpQYyH79AWABpCGNBtbb3sgrb9Qg5X29pdS41m5XhQWnEllJomISAPs88r2HsWFo_RtBq6_sEbrZ6fqDleE5BIKAaepYPCAY_t1ZFC5qlMXil8x_R6G0rf7aCYL0mrO6stmAQ='
 
     if debug:
         print(f'Message {message} sent to {receiver} at {datetime.now()} from {sender_session=}')
